fe255417ed52473d284bb89042e80b68a3a88178
sarant1 <samuel.arant10@gmail.com>
2023-06-17 13:38:54 -0400

    extract job title and job location

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup
 
+# find_all returns a resultSet (list of tags)
+# find returns a single tag
+
+
 # Is this bad ting to do ?
 url = "https://www.indeed.com/jobs?q=aws+engineer&l=Florida&from=searchOnHP&vjk=52d6942ea02fcc27"
 
@@ -18,12 +22,28 @@ def getPageData(driver):
     # soup = soup.prettify() # This turns soup into string
     return soup
 
-def writeResultSetToFile(resultSet):
-    with open('output.txt', 'a') as f:
-        for item in resultSet:
-            f.write(item.text + '\n')
+def extractJobLocation(job):
+    results = job.find_all(class_='companyLocation')
+    if len(results) == 0:
+        return "unknown"
+    else:
+        return results[0].text
 
-def writeDataToFile(data):
+def extractJobTitle(job):
+    results = job.select('span[id^="jobTitle"]')
+    if len(results) == 0:
+        return None
+    else:
+        return results[0].text
+
+def writeResultSetToFile(resultSet):
+    with open('output.txt', 'w') as f:
+        for item in resultSet:
+            item.prettify()
+            print(item)
+            # f.write(item + '\n' + '-------------------' + '\n')
+
+def writeStringDataToFile(data):
     with open('output.txt', 'w') as f:
         f.write(data)
 
@@ -34,22 +54,27 @@ def getJobTitles(soup):
 def getJobListItems(soup):
     return soup.find_all("ul", class_="jobsearch-ResultsList")
 
+# soup must include a unordered list of job conatiners
 def getJobListItem(soup):
     return soup.find_all("li")
     
 def main():    
     driver = setFireFoxDriver()
     page_soup = getPageData(driver)
+
+    # ul encapsures all job li items
     ul_of_job_containers = getJobListItems(page_soup)
-    job = getJobListItem(ul_of_job_containers[0])
-    print("List length: ", len(job))
-    writeDataToFile(job[0].prettify())
-    # for item in ul_of_job_containers:
-    #     job_title = getJobTitles(item)
-    #     print(job_title)
-    # driver.close()
-    # jobTitles = getJobTitles(page_soup)
-    # writeResultSetToFile(jobTitles)
+
+    # this will grab all li items and put them into a result set (list)
+    jobs = getJobListItem(ul_of_job_containers[0])
+
+    for job in jobs:
+        job_title = extractJobTitle(job)
+        job_location = extractJobLocation(job)
+        if job_title:
+            print(f'{job_title},{job_location}')
+    
+    driver.close()
     
 main()
     ## Close firefox
